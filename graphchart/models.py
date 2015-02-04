69442e553d21553a22d0cf8ce86cb7e1d1dfde4e
Larry Adames <adames.larry@gmail.com>
2015-02-04 15:33:37 -0500

    Updated code to match deployed code

--- graphchart/models.py
+++ graphchart/models.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django.db import models
-from django.utils import timezone
+from datetime import datetime
 
 class Reading(models.Model):
     temperature = models.IntegerField(default=0)
@@ -12,7 +12,7 @@ class Reading(models.Model):
         return name
 
     def was_read_recently(self):
-        now = timezone.now()
+        now = datetime.now()
         return now- datetime.timedelta(hours=1) <= self.reading_time<= now
         was_read_recently.admin_order_field = 'reading_time'
         was_read_recently.boolean = True
--- reading_call.py
+++ reading_call.py
@@ -1,14 +1,14 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'graphhackerati.settings')
 
-import django
-
 from datetime import datetime
 import requests
 from graphchart.models import Reading
 
-url = 'https://polar-dawn-2893.herokuapp.com/'
+url = 'http://polar-dawn-2893.herokuapp.com/'
 
 if __name__ == '__main__':
+    if Reading.objects.count() > 100:
+        Reading.objects.all().delete()
     res = requests.get(url).json()
-    Reading.objects.get_or_create(temperature=int(res['degrees']), reading_time=datetime.utcnow())
+    Reading.objects.get_or_create(temperature=int(res['degrees']), reading_time=datetime.now())
